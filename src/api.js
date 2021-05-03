e4324435ae4bd9234d5945da544a741b128d92fc
iwonafijal <fijal.iwona@gmail.com>
2021-05-03 20:38:25 +0200

    solve api key issue

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // Base URL
 
 const base_url = "https://api.rawg.io/api/";
-
+const API_KEY = process.env.REACT_APP_API_KEY;
 // Getting the date
 
 const getCurrentMonth = () => {
@@ -34,21 +34,24 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10 `;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10 `;
+const upcoming_games = `games?key=${API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
-export const popularGamesURL = () => `${base_url}${popular_games}`;
-export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
-export const newGamesURL = () => `${base_url}${newGames}`;
+export const popularGamesURL = () =>
+  `${base_url}${popular_games}?key=${API_KEY}`;
+export const upcomingGamesURL = () =>
+  `${base_url}${upcoming_games}?key=${API_KEY}`;
+export const newGamesURL = () => `${base_url}${newGames}?key=${API_KEY}`;
 
 // Game details
-export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${API_KEY}`;
 
 // Game Screenshoots
 export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots`;
+  `${base_url}games/${game_id}/screenshots?key=${API_KEY}`;
 
 // Searched Games
 export const searchGameURL = (game_name) =>
-  `${base_url}games?search=${game_name}&page_size=9`;
+  `${base_url}games?key=${API_KEY}&search=${game_name}&page_size=9`;
